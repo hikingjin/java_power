8acb1a3386fb1ded5d3fb356dff5ed0c5608b272
王瑾 <jin.wang@husor.com>
2019-01-06 12:47:54 +0800

    NO.1

---
+++ src/jin/wang/junior/a1_basic_data/Data01.java
@@ -0,0 +1,12 @@
+package jin.wang.junior.a1_basic_data;
+
+
+public class Data01 {
+    public static void main(String[] params){
+
+        //八种基本类型
+
+        byte b1= 21;//字节型
+        char c1= 21;//字符型
+    }
+}
